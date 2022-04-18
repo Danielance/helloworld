cb8836a0541f3cbd40bb28ed7f5f189bb6300780
Danielance <danielpiny@gmail.com>
2022-04-17 22:04:03 -0400

    Add files via upload

---
+++ Heck.java
@@ -0,0 +1,27 @@
+package funnyName;
+
+public class Heck {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("despacito");
+		System.out.println();
+		
+		int funnyNumber;
+		
+		funnyNumber = 42069;
+		
+		String stringRed = "red ";
+		String stringBlue = "blue ";
+		
+		for(int i = 1;i < 3; i++) {
+			System.out.println(i + " fish");
+		}
+		
+		System.out.println(stringRed + "fish");
+		System.out.println(stringBlue + "fish");
+		
+		
+		System.out.println(funnyNumber);
+	}
+}
